a29d404cdf95cb38215007b139bf8b87630cd5c1
Verity Gregory <verity.gregory@gmail.com>
2023-02-18 16:55:06 +0000

    working on components

--- src/components.py
+++ src/components.py
@@ -5,4 +5,23 @@ class Bell(Component):
         super().__init__(state, max_uses)
         
     def ring_bell(self):
-        return 'Ring! Ring!'
+        if self.current_state > 0:
+            print('Ring! Ring!')
+            self.current_state -= 2
+            return 'Ring! Ring!'
+        else:
+            print('This bell is broken and cannot be used')
+            return 'This bell is broken and cannot be used'
+
+class Brakes(Component):
+    def __init__(self, state, max_uses):
+        super().__init__(state, max_uses)
+    
+    def press_brakes(self):
+        if self.current_state > 0:
+            print('Squeak!')
+            self.current_state -= 3
+            return 'Squeak!'
+        else:
+            print("The brakes are broken and won't work")
+            return "The brakes are broken and won't work"
--- test/test_child_components.py
+++ test/test_child_components.py
@@ -1,5 +1,20 @@
-from src.components import Bell
+from src.components import *
 
 def test_Bell_has_a_ring_bell_method_that_returns_a_string():
     new_bell = Bell(100, 4)
     assert new_bell.ring_bell() == 'Ring! Ring!'
+
+def test_Bell_ring_method_reduces_component_state():
+    new_bell = Bell(100, 4)
+    new_bell.ring_bell()
+    assert new_bell.current_state == 98
+
+def test_If_bell_state_is_broken_does_not_reduce_state_and_returns_a_string():
+    new_bell = Bell(0, 3)
+    assert new_bell.ring_bell() == 'This bell is broken and cannot be used'
+    assert new_bell.current_state == 0
+
+def test_brakes_have_a_use_brakes_method_that_returns_a_string():
+    brakes = Brakes(100, 2)
+    assert brakes.press_brakes() == 'Squeak!'
+
